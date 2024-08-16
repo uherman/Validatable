5f3546d05c0d83a16632c6cb522e8c7b92e54f2d
Herman Brunberg <herman.brunberg@mio.se>
2024-08-16 23:52:20 +0200

    refactor(InvalidUserException): use default value for metadata

--- src/test/Validatable.Unit.Test/Exceptions/InvalidUserException.cs
+++ src/test/Validatable.Unit.Test/Exceptions/InvalidUserException.cs
@@ -3,7 +3,7 @@ using Validatable.Exceptions;
 namespace Validatable.Unit.Test.Exceptions;
 
 public class InvalidUserException(IReadOnlyCollection<ValidationException<ErrorCode?>> exceptions)
-    : AggregateValidationException<ErrorCode?>(exceptions, [])
+    : AggregateValidationException<ErrorCode?>(exceptions)
 {
     public override ErrorCode? Code { get; } = null;
 }
